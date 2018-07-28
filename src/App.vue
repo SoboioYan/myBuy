c25526ad559160e248e65d7b672d87d7d75729c7
杨延真 <786291632@qq.com>
2018-07-28 08:46:38 +0800

    完成首页数据渲染

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@
                     <div id="menu2" class="nav-box menuhd">
                         <ul>
                             <li class="index">
-                                <router-link to="/index">
+                                <router-link to="/index" class="router-link-exact-active ">
                                     <span class="out" style="top: 0px;">首页</span>
                                 </router-link>
                             
@@ -64,7 +64,7 @@
                                 </a>
                             </li>
                             <li class="goods">
-                                <a href="" class="router-link-exact-active ">
+                                <a href="">
                                     <span class="out" style="top: 0px;">购物商城</span>
                                 </a>
                             </li>
@@ -118,9 +118,35 @@
 </div>
 </template>
 <script>
-export default {};
+import $ from "jquery";
+export default {
+      mounted: function() {
+    $("#menu2 li a").wrapInner('<span class="out"></span>');
+    $("#menu2 li a").each(function() {
+      $('<span class="over">' + $(this).text() + "</span>").appendTo(this);
+    });
+    $("#menu2 li a").hover(
+      function() {
+        $(".out", this)
+          .stop()
+          .animate({ top: "48px" }, 300); // move down - hide
+        $(".over", this)
+          .stop()
+          .animate({ top: "0px" }, 300); // move down - show
+      },
+      function() {
+        $(".out", this)
+          .stop()
+          .animate({ top: "0px" }, 300); // move up - show
+        $(".over", this)
+          .stop()
+          .animate({ top: "-48px" }, 300); // move up - hide
+      }
+    );
+  }
+};
 </script>
 <style>
     /* 标准的 css中导入其他css的语法 */
-     @import url('./assets/statics/site/css/style.css');
+     @import url('./assets/statics/site/css/base.css');
 </style>
--- src/main.js
+++ src/main.js
@@ -4,16 +4,21 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 // 引入index组件
 import index from './components/index.vue'
+// 引入element ui 组件
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
 // 使用路由中间件
 Vue.use(VueRouter);
+// 使用element ui 中间件
+Vue.use(ElementUI);
+
 
 // 注册路由规则
 const router = new VueRouter({
   routes:[
-    {
-      path:'/index',
-      component:index
-    }
+    {path:'/',component:index},
+    {path:'/index',component:index},
   ]
 })
 
@@ -21,6 +26,7 @@ const router = new VueRouter({
 import './assets/statics/site/css/style.css';
 
 Vue.config.productionTip = false
+
 
 new Vue({
   // 选择器
